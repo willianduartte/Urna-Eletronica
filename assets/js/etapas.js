195af52f849684bce7473e485d9b2e80c4fa1632
Willian Duarte <willianduartedesouza04@gmail.com>
2022-04-11 14:47:08 -0300

    Projeto concluido

---
+++ assets/js/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: '38111',
+            nome: 'Fulano de Tal',
+            partido: 'ABC',
+            vice: '',
+            fotos: [
+                {url: '38111.jpg', legenda: 'Vereador'}
+            ]
+            },
+            {
+            numero: '77222',
+            nome: 'Beltrana da Silva',
+            partido: 'DEFG',
+            vice: '',
+            fotos: [
+                {url: '77222.jpg',legenda: 'Vereador'}
+            ]
+        }
+    ]
+        },
+        {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'luiz inacio lula da silva',
+                partido: 'Partido dos Trabalhadores',
+                vice: 'Geraldo Alckmin',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Presidente'},
+                    {url: '99_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Jair Bolsonaro',
+                partido: 'Partido Liberal',
+                vice: 'Braga Netto',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Presidente'},
+                    {url: '38111.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]   
+    }
+];
